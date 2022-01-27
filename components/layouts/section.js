f79301601d6e3955fa588720e1a689161f590cea
vikramadetya veer <89468629+vikramadetya@users.noreply.github.com>
2022-01-27 14:13:05 +0530

    changes copied
    changes copied

--- components/layouts/section.js
+++ components/layouts/section.js
@@ -1,21 +1,21 @@
 import { motion } from 'framer-motion'
-import { chakra, shouldForwardProp} from '@chakra-ui/react'
+import { chakra, shouldForwardProp } from '@chakra-ui/react'
 
 const StyledDiv = chakra(motion.div, {
-    shouldForwardProp: this.props => {
-        return shouldForwardProp(prop) || prop === 'transition'
-    }
+  shouldForwardProp: prop => {
+    return shouldForwardProp(prop) || prop === 'transition'
+  }
 })
 
-const Section = ({ childre, delay= 0}) => (
-    <StyledDiv
-    intial={{y: 10, opacity: 0}}
-    animate={{y: 0, opacity:1}}
-    transition={{duration: 0.8}, delay}
+const Section = ({ children, delay = 0 }) => (
+  <StyledDiv
+    initial={{ y: 10, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.8, delay }}
     mb={6}
-    >
-        {children}
-    </StyledDiv>
+  >
+    {children}
+  </StyledDiv>
 )
 
 export default Section
